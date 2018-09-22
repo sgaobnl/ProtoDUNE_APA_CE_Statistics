44f28c71bdfa2b28b1290b321133b8a2347fdcbe
Shanshan Gao <gao33.bnl@gmail.com>
2018-09-22 17:49:22 -0400

    update

---
+++ Abnormals.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+File Name: init_femb.py
+Author: GSS
+Mail: gao.hillhill@gmail.com
+Description: 
+Created Time: 7/15/2016 11:47:39 AM
+Last modified: Fri Sep 21 23:44:58 2018
+"""
+
+def Abnormals( ):
+    Inactive_FEs = ["APA3WIB4FEMB2FE7", "APA4WIB3FEMB2FE0","APA5WIB1FEMB1FE1","APA5WIB1FEMB1FE4","APA5WIB1FEMB0FE4",]
+    Ab_Phase_ADCs = ["APA5WIB4FEMB3ADC6","APA5WIB2FEMB1ADC6","APA5WIB4FEMB0ADC6","APA5WIB2FEMB3ADC6",
+                     "APA6WIB4FEMB3ADC6","APA6WIB3FEMB1ADC6","APA6WIB0FEMB0ADC6"]
+    return Inactive_FEs , Ab_Phase_ADCs 
+
+
